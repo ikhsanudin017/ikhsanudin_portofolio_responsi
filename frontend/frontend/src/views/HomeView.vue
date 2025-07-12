6b90db090998ff3ecc4c71d0ec1d3676863aea1c
ikhsanudin017 <ikhsankuliah017@gmail.com>
2025-07-12 10:15:37 +0700

    commit repo baru

---
+++ frontend/frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <Hero />
+    <Education />
+    <Skills />
+    <Projects />
+    <Contact />
+  </main>
+</template>
+
+<script setup>
+import Hero from '../components/Hero.vue';
+import Education from '../components/Education.vue';
+import Skills from '../components/Skills.vue';
+import Projects from '../components/Projects.vue';
+import Contact from '../components/Contact.vue';
+</script>
