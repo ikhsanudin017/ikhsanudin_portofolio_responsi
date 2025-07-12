68c4e7b28eaa29b6844591053aa55b4d691b14fd
ikhsanudin017 <ikhsankuliah017@gmail.com>
2025-07-12 10:47:32 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -31,5 +31,12 @@ res.status(200).json(rows);
 res.status(500).json({ error: 'Gagal mengambil data proyek' });
 }
 });
-// Wajib ada agar Vercel bisa menjalankan backend
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
 module.exports = app;
